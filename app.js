443dce0c6d843909d00fb7a7220ffa8504487a1e
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-26 22:00:30 +0000

    f1

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const sum = (a,b) => {
-    return a + b
+const fromYenToPound = function(valueInYen) {
+let valueInPound = valueInYen * 0.87;
+return valueInPound;}
+const fromDollarToYen = function(valueInDollar) {
+let valueInYen = valueInDollar * 156.5;
+return valueInYen;
 }
-module.exports = sum ;
+const fromEuroToDollar = function(valueInEuro) {
+let valueInDollar = valueInEuro * 1.07;
+return valueInDollar;
+}
+const sum = (a,b) => {
+return a + b;
+}
+console.log(sum(7,3))
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
